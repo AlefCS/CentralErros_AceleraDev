76b84c0eaea8981ad0c0d880fb94d509e8f72417
Alef Carneiro <carneiroalef@gmail.com>
2020-07-17 19:21:29 -0300

    Project settings updated

--- errors_center/settings.py
+++ errors_center/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    # Our apps
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django REST framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
